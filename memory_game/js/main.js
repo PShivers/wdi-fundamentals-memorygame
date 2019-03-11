90e7f0932becc93cd27bb58ddc85a1730f68e1b1
Paul Shivers <paulshivers@Pauls-MacBook-Pro-2.local>
2019-03-11 17:57:33 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,23 @@
-console.log("Up and running!");
 
-// console.log ("User flipped " + "cardOne");
-// console.log ("User flipped " + "cardThree");
-
-var cards = ["queen", "queen", "king", "king"];
+var cards = ["Queen", "Queen", "King", "King"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push("cardOne");
-console.log ("User flipped queen")
-
-var cardTwo = cards[1];
-cardsInPlay.push("cardTwo");
-console.log ("User flipped king");
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else if (cardsInPlay[0] !== cardsInPlay[1]) {
-		alert("Sorry, try again.");
-	}
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+  } else {
+  console.log("Sorry, try again.");
+  }
 }
 
-
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+}
+  
+flipCard(0);
+flipCard(2);
